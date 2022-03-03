b4033b6660963a53b0ec1f87693baca7447b32d0
taha <tahaelkholy.dev@gmail.com>
2022-03-03 19:20:53 +0200

    upload todo

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:todo/layout/home_layout.dart';
+import 'package:todo/shared/bloc_observer/bloc_observer.dart';
+import 'package:todo/shared/styles/colors.dart';
+
+void main() {
+  // use bloc observer for debug the app in any time
+  BlocOverrides.runZoned(
+        () {
+      runApp(
+         MyApp(),
+      );
+    },
+    blocObserver: MyBlocObserver(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
